61be745fad4e6ab1a7b785d6c06519c3b9e649fc
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-04 13:58:56 -0300

    Convertendo Metros

---
+++ estrutura-sequencial/exercicio-5.js
@@ -0,0 +1,6 @@
+// 5 - Faça um Programa que converta metros para centímetros.
+
+const metros = parseInt(prompt("Escreva Quantos Metros deseja Calcular : "))
+const centimetros = metros * 100 
+
+console.log(`${metros} Metros convertidos para Centimetros dao : ${centimetros} Centimetros`)
