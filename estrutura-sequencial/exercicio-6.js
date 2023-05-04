dbff93a31758360f5b4a359919d0f69d56159d65
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-04 14:01:56 -0300

    Calculando Area do Circulo

---
+++ estrutura-sequencial/exercicio-6.js
@@ -0,0 +1,6 @@
+// 6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+const raio = parseInt(prompt("Escreva O Raio do Circulo a ser Calculado : "))
+const area = 3.14 * (raio * raio)
+
+console.log(`A Area do Circulo com Raio ${raio} é : ${area}`)
