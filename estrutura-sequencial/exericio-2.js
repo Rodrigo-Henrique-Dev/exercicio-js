ab31a386e8dc45e7a79fd3fdaa2a32b38a12de58
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-04 13:39:33 -0300

    Gerenciando Mensagem

---
+++ estrutura-sequencial/exericio-2.js
@@ -0,0 +1,5 @@
+// 2 - Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+const numero = prompt("Escreva O numero que deseja printar na Tela : ")
+console.log(` O numero selecionado é : ${numero} `)
+
