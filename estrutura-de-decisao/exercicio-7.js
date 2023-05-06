e5d821b7ac2316ba04a0c1977441331e111caff8
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-06 18:42:22 -0300

    Maior ou Menor 3 Numeros

---
+++ estrutura-de-decisao/exercicio-7.js
@@ -0,0 +1,27 @@
+/*
+    7 - Faça um Programa que leia três números e mostre o maior e o menor deles.
+*/
+const numero1 = parseInt(prompt(" Escreva o Primeiro Numero : "))
+const numero2 = parseInt(prompt(" Escreva o Segundo Numero : "))
+const numero3 = parseInt(prompt(" Escreva o Terceiro Numero : "))
+let maior = numero1
+let menor = numero1
+
+if (maior < numero2) {
+    maior = numero2
+}
+
+if (maior < numero3) {
+    maior = numero3
+}
+
+if (menor > numero2) {
+    menor = numero2
+}
+
+if (menor > numero3) {
+    menor = numero3
+}
+
+console.log(`O maior numero é o : ${maior}` +
+    `O menor numero é o : ${menor}`)
