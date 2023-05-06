a4d0a68aedb122db5feeee58d79fe7504cffc5ff
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-06 19:46:29 -0300

    Calculando Media

--- estrutura-sequencial/exercicio-4.js
+++ estrutura-sequencial/exercicio-4.js
@@ -1,9 +1,9 @@
 // 4 - Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 
-let soma = 0 
-for(let i = 1; i < 4; i++){
+let soma = 0
+for (let i = 0; i < 4; i++) {
 	let notas = parseInt(prompt(`Escreva Sua ${i} Nota : `))
-	soma = soma + notas 
+	soma = soma + notas
 }
 
 let media = soma / 4
