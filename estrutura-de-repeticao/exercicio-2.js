36e5fe3f78f18ec857a3c8886bb3d0c2f997d52a
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-08 20:21:59 -0300

    Usuario e Senha

--- estrutura-de-repeticao/exercicio-2.js
+++ estrutura-de-repeticao/exercicio-2.js
@@ -2,12 +2,10 @@
     2 - Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
 */
 
-let usuario = prompt(" Escreva o seu Usuario : ").toLowerCase()
-let senha = prompt("Escreva a sua Senha : ").toLowerCase()
+let nota = parseInt(prompt(" Escreva a Nota : "))
 
-for (let i = 0; usuario === senha; i++) {
-    usuario = prompt(" Escreva um Login Diferente da Senha :").toLowerCase()
-    senha = prompt("Escreva sua Senha Diferente do Login :").toLowerCase()
+for (let i = 0; nota < 0 || nota > 10; i++) {
+    nota = parseInt(prompt(" Escreva a Nota valida entre (0 a 10):"))
 }
 
-console.log("Seu cadastro Foi Criado : ")
+console.log(`Sua nota foi : ${nota}`)
