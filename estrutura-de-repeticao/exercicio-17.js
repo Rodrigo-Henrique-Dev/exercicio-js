33152bcfd436b185779b911e8f49d4af45d31f96
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-10 14:51:51 -0300

    Calculo Fatorial

--- estrutura-de-repeticao/exercicio-17.js
+++ estrutura-de-repeticao/exercicio-17.js
@@ -2,7 +2,7 @@
     17 - Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário. Ex.: 5!=5.4.3.2.1=120
 */
 
-const numero = parseInt(prompt("Escreva o Numero que Deseja Fatorar : ") || "")
+const numero = parseInt(prompt("Escreva o Numero que Deseja Fatorar : "))
 let resultado = 1
 let i = 1
 
