a07419eac8c7ff760a89ad54cd543fcc0b7c9493
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-10 12:23:48 -0300

    Intervalo entre 2 Numeros

--- estrutura-de-repeticao/exericio-10.js
+++ estrutura-de-repeticao/exericio-10.js
@@ -2,3 +2,16 @@
     10 - Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
 */
 
+let n1 = parseInt(prompt("Escreva um Numero : "))
+let n2 = parseInt(prompt("Escreva outro Numero : "))
+
+if (n1 > n2) {
+    let box = n1
+    n1 = n2
+    n2 = box
+}
+
+while (n1 <= n2) {
+    console.log(n1)
+    n1 = n1 + 1
+}
