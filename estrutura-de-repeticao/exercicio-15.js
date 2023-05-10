e83f318c1afe2fede64e2baab1f1171c869c2323
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-10 14:32:46 -0300

    Fibonacci n-esimo Termo

--- estrutura-de-repeticao/exercicio-15.js
+++ estrutura-de-repeticao/exercicio-15.js
@@ -1,5 +1,5 @@
 /*    
-    15 - A série de Fibonacci é formada pela seqüência 0,1,1,2,3,5,8,13,21,34,55,... Faça um programa que gere a série até que o valor seja maior que 500.
+    15 - A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série até o n−ésimo termo.
 */
 
 const numero = parseInt(prompt("Escreva ate que termo voce quer a Sequencia : "))
