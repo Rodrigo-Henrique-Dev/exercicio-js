531035503975224d29f17d08090eade6aadc013d
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-10 00:22:28 -0300

    De 1 ate 50 Impar

---
+++ estrutura-de-repeticao/exercicio-9.js
@@ -0,0 +1,9 @@
+/*
+    9 - Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+*/
+
+for (let n1 = 0; n1 < 50; n1++) {
+    if (n1 % 2 > 0) {
+        console.log(`O numero ${n1} é IMPAR`)
+    }
+}
