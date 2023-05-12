ce071dd2cf138601b04d7b77d1972a88e83fe33c
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-12 18:43:58 -0300

    Maior Numero

---
+++ estrutura-de-repeticao/exercicio-20.js
@@ -0,0 +1,18 @@
+/*    
+    20 - Altere o programa de cálculo do fatorial, permitindo ao usuário calcular o fatorial várias vezes e limitando o fatorial a números inteiros positivos e menores que 16.
+*/
+const vezes = parseInt(prompt("Escreva quantos Numeros Deseja Fatorar : "))
+
+for (let v = 0; v < vezes; v++) {
+    let numero = parseInt(prompt("Escreva o Numero que Deseja Fatorar : "))
+    let resultado = 1
+    let i = 1
+    while (numero < 0 || numero > 16) {
+        numero = parseInt(prompt("Escreva um Numero Valido para Fatorar(0 ate 16) : "))
+    }
+    while (i <= numero) {
+        resultado *= i
+        i += 1
+    }
+    console.log(`O Fatorial do numero ${numero} é : ${resultado}`)
+}
--- estrutura-de-repeticao/exercicio-7.js
+++ estrutura-de-repeticao/exercicio-7.js
@@ -2,12 +2,14 @@
     7 - Faça um programa que leia 5 números e informe o maior número.
 */
 
-let max = 0
+let max = null
 let numeros = []
-for (let contador = 1; contador <= 5; contador++) {
+for (let contador = 0; contador < 5; contador++) {
     let numero = parseInt(prompt("Escreva um Numero : "))
     numeros.push(numero)
-    max = console.log(Math.max(numeros))
+    if (max === null || numero > max) {
+        max = numero
+    }
 }
 console.log(typeof (numeros))
 console.log(`O maior numero é o : ${max}`)
