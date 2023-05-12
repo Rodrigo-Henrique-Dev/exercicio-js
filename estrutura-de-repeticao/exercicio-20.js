1ee45112af386934853def7bbec06786e11cfc57
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-12 18:45:45 -0300

    Fatorial Limitada

--- estrutura-de-repeticao/exercicio-20.js
+++ estrutura-de-repeticao/exercicio-20.js
@@ -1,6 +1,7 @@
 /*    
     20 - Altere o programa de cálculo do fatorial, permitindo ao usuário calcular o fatorial várias vezes e limitando o fatorial a números inteiros positivos e menores que 16.
 */
+
 const vezes = parseInt(prompt("Escreva quantos Numeros Deseja Fatorar : "))
 
 for (let v = 0; v < vezes; v++) {
