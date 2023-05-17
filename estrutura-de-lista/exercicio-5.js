dfb0fa355d63c09aab6b4bb8d8f80bd520fc4033
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-17 19:30:13 -0300

    Ler 3 Vetores

--- estrutura-de-lista/exercicio-5.js
+++ estrutura-de-lista/exercicio-5.js
@@ -1,20 +1,25 @@
 /*
     5 - Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
 */
-let par = ""
-let impar = ""
-let numero = ""
+
+let par = []
+let impar = []
+let numeros = []
 for (let i = 0; i < 20; i++) {
-    numero = parseInt(prompt("Escreva um Numero : "))
+    let numero = parseInt(prompt("Escreva um Numero : "))
     if (numero % 2 === 0) {
-        par = par + " - " + numero.toString()
+        par.push(numero)
+        numeros.push(numero)
     } else {
-        impar = impar + " - " + numero.toString()
+        impar.push(numero)
+        numeros.push(numero)
     }
 }
 
-console.log(`Numeros Pares : ${par}\n Numeros Impares : ${impar}`)
 
-/* 
-Como fazer com os Numeros o mesmo que o Par e Impar
-*/
+
+console.log(`Numeros Pares : ${par}\n` +
+    `Numeros Impares : ${impar}\n` +
+    `Numeros : ${numeros}`
+)
+
