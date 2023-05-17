d55dd4b04dcedf74157769356cea97a86a18bcc8
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-17 13:56:49 -0300

    Intercalando 3 Vetores

---
+++ estrutura-de-lista/exercicio-11.js
@@ -0,0 +1,21 @@
+/*    
+    11 - Altere o programa anterior, intercalando 3 vetores de 10 elementos cada.
+*/
+
+let vetor = []
+for (let i = 0; i < 10; i++) {
+    vetor.push(prompt("Escreva um Numero : "))
+}
+console.log(vetor)
+
+let vetor2 = []
+for (let i2 = 0; i2 < 10; i2++) {
+    vetor2.push(prompt("Escreva um Numero : "))
+}
+console.log(vetor2)
+
+let vetor3 = []
+for (let i3 = 0; i3 < 10; i3++) {
+    vetor3.push(prompt("Escreva um Numero : "))
+}
+console.log(vetor3)
