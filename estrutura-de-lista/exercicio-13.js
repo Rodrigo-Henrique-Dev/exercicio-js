401f367f2dee0eadadb529f0a99e12a28f6d7938
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-19 12:46:19 -0300

    Base Expoente

---
+++ estrutura-de-lista/exercicio-13.js
@@ -0,0 +1,14 @@
+/*
+    13 - Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao segundo número. Não utilize a função de potência da linguagem.
+*/
+
+let base = parseInt(prompt("Escreva a Base : "))
+let expoente = parseInt(prompt("Escreva o Expoente : "))
+let resultado = 1
+
+for (let i = 1; i <= expoente; i++) {
+    resultado = base * base
+    resultado = base * resultado
+}
+
+console.log({ RESULTADO: resultado })
