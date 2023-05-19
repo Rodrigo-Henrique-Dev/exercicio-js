04202958ba14aedac24c382d3b5c45038bea5316
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-19 11:46:32 -0300

    Elemento Intercalado

--- estrutura-de-lista/exercicio-10.js
+++ estrutura-de-lista/exercicio-10.js
@@ -2,17 +2,30 @@
     10 - Faça um Programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20 elementos, cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
 */
 
-let vetor = []
-for (let i = 0; i < 10; i++) {
-    vetor.push(prompt("Escreva um Numero : "))
+let par = []
+let impar = []
+let numeros = []
+let contadorPar = 0
+let contadorImpar = 0
+for (let i = 0; i < 20; i++) {
+    const numero = parseInt(prompt("Escreva um Numero : "))
+    if (numero % 2 === 0) {
+        par.push(numero)
+    } else {
+        impar.push(numero)
+    }
 }
-console.log(vetor)
+for (let i2 = 0; i2 < 20; i2++) {
+    if (i2 % 2 === 0) {
+        numeros = par[contadorPar]
+        contadorPar++
+    } else {
+        numeros = impar[contadorImpar]
+        contadorImpar++
+    }
+}
+console.log(numeros)
 
-let vetor2 = []
-for (let indice = 0; indice < 10; i++) {
-    vetor2.push(prompt("Escreva um Numero : "))
-}
-console.log(vetor2)
 
 /*
 como intercalar os dois vetores 
