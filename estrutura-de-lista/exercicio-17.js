d2dd7727b30a4e80d6babdcb2c28b197862ab109
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-22 18:31:16 -0300

    Salto em Distancia

--- estrutura-de-lista/exercicio-17.js
+++ estrutura-de-lista/exercicio-17.js
@@ -1,13 +1,14 @@
 /*
     17 - Em uma competição de salto em distância cada atleta tem direito a cinco saltos. O resultado do atleta será determinado pela média dos cinco valores restantes. Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas pelo atleta em seus saltos e depois informe o nome, os saltos e a média dos saltos. O programa deve ser encerrado quando não for informado o nome do atleta. A saída do programa deve ser conforme o exemplo abaixo:
 */
+
 let soma = 0
 let array = []
 let media = 0
 let nome = ""
 nome = prompt("Escreva o nome do atleta : ")
 while (nome === "") {
-    nome = prompt("Escreva um nome valido do atleta : ")
+    return;
 }
 for (let i = 0; i < 5; i++) {
     let salto = parseInt(prompt("Escreva a Altura do Salto : "))
