0f52e83085b80f0894007a422f8dd410d28f9e4e
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-22 12:17:19 -0300

    Media de Salto

---
+++ estrutura-de-lista/exercicio-17.js
@@ -0,0 +1,15 @@
+/*
+    17 - Em uma competição de salto em distância cada atleta tem direito a cinco saltos. O resultado do atleta será determinado pela média dos cinco valores restantes. Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas pelo atleta em seus saltos e depois informe o nome, os saltos e a média dos saltos. O programa deve ser encerrado quando não for informado o nome do atleta. A saída do programa deve ser conforme o exemplo abaixo:
+*/
+let soma = 0
+let array = []
+let media = 0
+
+const nome = prompt("Escreva o nome do atleta : ")
+for (let i = 0; i < 5; i++) {
+    let salto = parseInt(prompt("Escreva a Altura do Salto : "))
+    soma = soma + salto
+    array.push(salto)
+    media = salto / 5
+}
+console.log({ nome: nome, salto: array, soma: soma, media: media })
