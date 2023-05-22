3347be82d455eb1614031e40dc0fd9f70f1ac094
Rodrigo 'xRod' Henrique <103092765+xRod1708@users.noreply.github.com>
2023-05-22 19:00:33 -0300

    Notas Indeterminadas

--- estrutura-de-lista/exercicio-15.js
+++ estrutura-de-lista/exercicio-15.js
@@ -1,15 +1,40 @@
-/*    
-    15 - A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série até o n−ésimo termo.
+/*
+    15 - Faça um programa que leia um número indeterminado de valores, correspondentes a notas, encerrando a entrada de dados quando for informado um valor igual a -1 (que não deve ser armazenado). Após esta entrada de dados, faça:
+    Mostre a quantidade de valores que foram lidos;
+    Exiba todos os valores na ordem em que foram informados, um ao lado do outro;
+    Exiba todos os valores na ordem inversa à que foram informados, um abaixo do outro;
+    Calcule e mostre a soma dos valores;
+    Calcule e mostre a média dos valores;
+    Calcule e mostre a quantidade de valores acima da média calculada;
+    Calcule e mostre a quantidade de valores abaixo de sete;
+    Encerre o programa com uma mensagem;
 */
 
-const numero = parseInt(prompt("Escreva ate que termo voce quer a Sequencia : "))
-let a = 0
-let b = 1
-let resultado = 0
+let notas = [0]
+let contadorCima = 0
+let contadorBaixo = 0
+let posicao = 0
+let inverso = null
+let soma = 0
+let media = 0
 
-for (let i = 1; i < numero; i++) {
-    resultado = a + b;
-    a = b
-    b = resultado
-    console.log(resultado)
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = parseFloat(prompt("Escreva uma nota :"))
+    notas.push(nota)
+    if (nota === -1) {
+        break;
+    }
+    soma = soma + nota
+    media = soma / i
+    if (media < 7) {
+        contadorBaixo++
+    } else {
+        contadorCima++
+    }
+    posicao = notas.length()
+    inverso = notas.reverse()
 }
+console.log({ array: notas, soma: soma, media: media, abaixo: contadorBaixo, acima: contadorCima })
+
+/* por que nao consigo usar o notas.length */
