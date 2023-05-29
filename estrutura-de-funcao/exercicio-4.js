01b63cba0dd0d5bfb215d975faa62fc779c687f4
Rodrigo Henrique <103092765+Rodrigo-Henrique-Dev@users.noreply.github.com>
2023-05-29 13:30:36 -0300

    Positivo e Negativo

---
+++ estrutura-de-funcao/exercicio-4.js
@@ -0,0 +1,14 @@
+/*
+   4 - Faça um programa, com uma função que necessite de um argumento. A função retorna o valor de caractere ‘P’, se seu argumento for positivo, e ‘N’, se seu argumento for zero ou negativo.
+*/
+
+function argumento(n) {
+    if (n <= 0) {
+        numero = "negativo"
+    } else {
+        numero = "positivo"
+    }
+    return numero
+}
+
+console.log(argumento(1))
