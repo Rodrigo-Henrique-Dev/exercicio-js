47831a33fdb20c8e1239e2066e6a291f99912029
Rodrigo Henrique <103092765+Rodrigo-Henrique-Dev@users.noreply.github.com>
2023-05-29 13:26:23 -0300

    Soma 3 Numeros

---
+++ estrutura-de-funcao/exercicio-3.js
@@ -0,0 +1,10 @@
+/*
+    3 - Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+*/
+
+function soma(n1, n2, n3) {
+    let soma = n1 + n2 + n3
+    return soma
+}
+
+console.log(soma(3, 4, 5))
