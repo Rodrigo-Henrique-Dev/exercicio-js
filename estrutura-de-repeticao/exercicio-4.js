b7dd68f56a2b9f725fb7ed1ae4d9ad9da3b1048d
Rodrigo Henrique <103092765+Rodrigo-Henrique-Dev@users.noreply.github.com>
2023-06-12 12:53:05 -0300

    Crescimento do Pais

--- estrutura-de-repeticao/exercicio-4.js
+++ estrutura-de-repeticao/exercicio-4.js
@@ -2,15 +2,15 @@
     4 - Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%.Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
 */
 
-let populacaoA = 80000
-let populacaoB = 200000
-let crescimentoA = 0.03
-let crescimentoB = 0.015
-let ano = 0
-while (populacaoA <= populacaoB) {
-    populacaoA = parseInt(populacaoA + (populacaoA * crescimentoA))
-    populacaoB = parseInt(populacaoB + (populacaoB * crescimentoB))
-    ano++
-}
+let paisA = 80000
+let paisB = 200000
+let taxaA = 0.03
+let taxaB = 0.015
+let anos = 0
 
-console.log(`Serao ${ano} Anos ate a Populacao A ultrapassar/igualar a Populacao B`)
+while (paisA <= paisB) {
+    paisA = paisA + (paisA * taxaA)
+    paisB = paisB + (paisB * taxaB)
+    anos++
+}
+console.log(`Durará ${anos} anos para que o Pais-A chegue no Pais-B`)
