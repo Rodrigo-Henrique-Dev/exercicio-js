77b80c87d37f760ebf3ab59e16881bfdb4ad26ea
Rodrigo Henrique <103092765+Rodrigo-Henrique-Dev@users.noreply.github.com>
2023-06-14 13:21:37 -0300

    Numeros Primos

--- estrutura-de-repeticao/exercicio-35.js
+++ estrutura-de-repeticao/exercicio-35.js
@@ -1,3 +1,13 @@
 /*
     35 - Encontrar números primos é uma tarefa difícil. Faça um programa que gera uma lista dos números primos existentes entre 1 e um número inteiro informado pelo usuário.
 */
+
+let primos = []
+const numero = parseInt(prompt("Escreva ate onde deseja Verificar : "))
+
+for (let i = 2; i < numero; i++) {
+    if (i % 2 !== 0) {
+        primos.push(i)
+    }
+}
+console.log(primos)
