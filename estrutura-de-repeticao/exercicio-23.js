a8d9bb85eb0ed13b261b16fbab31ee5fcc06a4a4
Rodrigo Henrique <103092765+Rodrigo-Henrique-Dev@users.noreply.github.com>
2023-06-14 12:57:10 -0300

    Primos e Divisoes

--- estrutura-de-repeticao/exercicio-23.js
+++ estrutura-de-repeticao/exercicio-23.js
@@ -1,6 +1,20 @@
 /*    
     23 - Faça um programa que mostre todos os primos entre 1 e N sendo N um número inteiro fornecido pelo usuário. O programa deverá mostrar também o número de divisões que ele executou para encontrar os números primos. Serão avaliados o funcionamento, o estilo e o número de testes (divisões) executados.
 */
+let contadorPrimo = 0
+let divisoes = 0
+let ehPrimo = true
+const n1 = 1
+const n2 = parseInt(prompt("Escreva um Numero Inteiro : "))
 
+for (let i = 0; i < n2; i++) {
+    if (i % 2 !== 0) {
+        ehPrimo = false
+        divisoes++
+    } else {
+        contadorPrimo++
+        divisoes++
+    }
+}
 
-const nPrimo = parseInt(prompt("Escreva ate que numero deseja Verificar : "))
+console.log({ divisoes, contadorPrimo })
